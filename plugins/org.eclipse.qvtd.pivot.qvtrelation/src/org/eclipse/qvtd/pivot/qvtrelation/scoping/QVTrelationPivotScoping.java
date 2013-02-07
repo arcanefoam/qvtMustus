87eb905eb85f6fb20a4632e4f243aaed8fd5a03b
Horacio Hoyos <hhr502@york.ac.uk>
2013-02-07 14:17:55 +0000

    Revert "Fixed merging conflicts"
    This reverts commit b718458c7eb3b5dd4b60dd44540c66f743b2bdb3.

--- plugins/org.eclipse.qvtd.pivot.qvtrelation/src/org/eclipse/qvtd/pivot/qvtrelation/scoping/QVTrelationPivotScoping.java
+++ plugins/org.eclipse.qvtd.pivot.qvtrelation/src/org/eclipse/qvtd/pivot/qvtrelation/scoping/QVTrelationPivotScoping.java
@@ -1,3 +1,41 @@
+<<<<<<< HEAD
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2012 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *
+ * </copyright>
+ */
+package org.eclipse.qvtd.pivot.qvtrelation.scoping;
+
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.ocl.examples.pivot.scoping.Attribution;
+import org.eclipse.qvtd.pivot.qvtrelation.QVTrelationPackage;
+import org.eclipse.qvtd.pivot.qvtrelation.attributes.KeyAttribution;
+import org.eclipse.qvtd.pivot.qvtrelation.attributes.RelationAttribution;
+import org.eclipse.qvtd.pivot.qvtrelation.attributes.RelationDomainAttribution;
+import org.eclipse.qvtd.pivot.qvtrelation.attributes.RelationModelAttribution;
+
+public class QVTrelationPivotScoping
+{	
+	public static void init() {
+		Map<EClassifier, Attribution> registry = Attribution.REGISTRY;
+		registry.put(QVTrelationPackage.Literals.KEY, KeyAttribution.INSTANCE);
+		registry.put(QVTrelationPackage.Literals.RELATION, RelationAttribution.INSTANCE);
+		registry.put(QVTrelationPackage.Literals.RELATION_DOMAIN, RelationDomainAttribution.INSTANCE);
+		registry.put(QVTrelationPackage.Literals.RELATION_MODEL, RelationModelAttribution.INSTANCE);
+	}
+}
+=======
 /**
  * <copyright>
  *
@@ -63,3 +101,4 @@ public class QVTrelationPivotScoping
 		EnvironmentView.addDisambiguator(Function.class, new FunctionDisambiguator());
 	}
 }
+>>>>>>> refs/remotes/upstream/master
--- plugins/org.eclipse.qvtd.xtext.qvtrelation/emf-gen/org/eclipse/qvtd/xtext/qvtrelationcst/AbstractDomainCS.java
+++ plugins/org.eclipse.qvtd.xtext.qvtrelation/emf-gen/org/eclipse/qvtd/xtext/qvtrelationcst/AbstractDomainCS.java
@@ -1,3 +1,38 @@
+<<<<<<< HEAD
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2012 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.eclipse.qvtd.xtext.qvtrelationcst;
+
+import org.eclipse.ocl.examples.pivot.util.Nameable;
+import org.eclipse.ocl.examples.xtext.base.baseCST.ModelElementCS;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Abstract Domain CS</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.eclipse.qvtd.xtext.qvtrelationcst.QVTrelationCSTPackage#getAbstractDomainCS()
+ * @model abstract="true" superTypes="org.eclipse.ocl.examples.xtext.base.baseCST.ModelElementCS org.eclipse.ocl.examples.pivot.Nameable"
+ * @generated
+ */
+public interface AbstractDomainCS extends ModelElementCS, Nameable {
+} // AbstractDomainCS
+=======
 /**
  * <copyright>
  *
@@ -30,3 +65,4 @@ import org.eclipse.ocl.examples.xtext.base.baseCST.ModelElementCS;
  */
 public interface AbstractDomainCS extends ModelElementCS, org.eclipse.ocl.examples.domain.elements.Nameable {
 } // AbstractDomainCS
+>>>>>>> refs/remotes/upstream/master
