b718458c7eb3b5dd4b60dd44540c66f743b2bdb3
Horacio Hoyos <hhr502@york.ac.uk>
2013-02-07 12:15:34 +0000

    Fixed merging conflicts

--- tests/org.eclipse.qvtd.xtext.qvtrelation.tests/src/org/eclipse/qvtd/xtext/qvtrelation/tests/LoadTests.java
+++ tests/org.eclipse.qvtd.xtext.qvtrelation.tests/src/org/eclipse/qvtd/xtext/qvtrelation/tests/LoadTests.java
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-/**
- * <copyright>
- *
- * Copyright (c) 2012 E.D.Willink and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     E.D.Willink - initial API and implementation
- *
- * </copyright>
- */
-package org.eclipse.qvtd.xtext.qvtrelation.tests;
-
-import java.io.IOException;
-
-import org.eclipse.ocl.examples.xtext.essentialocl.services.EssentialOCLLinkingService;
-import org.eclipse.qvtd.xtext.qvtbase.tests.LoadTestCase;
-import org.eclipse.qvtd.xtext.qvtrelation.QVTrelationStandaloneSetup;
-
-/**
- * Tests that load a model and verify that there are no unresolved proxies as a result.
- */
-public class LoadTests extends LoadTestCase
-{	
-	@Override
-	protected void setUp() throws Exception {
-		EssentialOCLLinkingService.DEBUG_RETRY = true;
-		super.setUp();
-		QVTrelationStandaloneSetup.doSetup();
-	}
-
-	public void testLoad_Keys_qvtr() throws IOException, InterruptedException {
-		doLoad_Concrete("Keys", "qvtr");
-	}	
-
-	public void testLoad_hstmtostm_qvtr() throws IOException, InterruptedException {
-		doLoad_Concrete("hstmtostm", "qvtr");
-	}	
-
-//	public void testLoad_RelToCoreWithoutErrors_qvtr() throws IOException, InterruptedException {
-//		doLoad_Concrete("RelToCoreWithoutErrors", "qvtr");
-//	}	
-}
-=======
 /**
  * <copyright>
  *
@@ -150,4 +102,3 @@ public class LoadTests extends LoadTestCase
 		doLoad_Concrete(URI.createPlatformResourceURI("/org.eclipse.qvtd.examples.qvtrelation.modelmorf/qvtrsrc/UmlToRel/UmlToRel.qvtr", true));
 	}	
 }
->>>>>>> refs/remotes/upstream/master
