0341397b313977857adba52411f117fb7684a7dd
Horacio Hoyos <hhr502@york.ac.uk>
2013-02-15 13:17:21 +0000

    Finished polishing up the QVTc DomainManager. The idea is that the domain manager is responsible for element look up and insertion so the visitor is decoupled from the model technology, i.e., we could have a QVTcEMFDomainManager, a QVTcCSVDomainManager, etc..

--- plugins/uk.ac.york.qvtd.library/src/uk/ac/york/qvtd/library/executor/QVTcDomainManager.java
+++ plugins/uk.ac.york.qvtd.library/src/uk/ac/york/qvtd/library/executor/QVTcDomainManager.java
@@ -13,19 +13,24 @@ package uk.ac.york.qvtd.library.executor;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.ocl.examples.domain.elements.DomainType;
 import org.eclipse.ocl.examples.domain.evaluation.DomainModelManager;
+import org.eclipse.ocl.examples.pivot.Type;
 import org.eclipse.qvtd.pivot.qvtbase.TypedModel;
 
 
+// TODO: Auto-generated Javadoc
 /**
  * QVTc Domain Manager is the class responsible for managing the QVTc virtual
  * machine meta-models and models. 
@@ -35,6 +40,7 @@ import org.eclipse.qvtd.pivot.qvtbase.TypedModel;
 public class QVTcDomainManager implements DomainModelManager {
     
     
+    /** The Constant MIDDLE_MODEL. */
     public static final TypedModel MIDDLE_MODEL = null;
     
 	// TODO how to manage aliases?
@@ -43,28 +49,24 @@ public class QVTcDomainManager implements DomainModelManager {
 
 	/**
 	 * Instantiates a new QVTc Domain Manager. Responsible for creating new
-	 * instances of the middle model and the middle model EFactory. 
-	 * 
-	 * @param middleMetamodel the middle metamodel
+	 * instances of the middle model and the middle model EFactory.
+	 *
 	 */
 	public QVTcDomainManager() {
 	    // null entries in the modelMap and pivotMap will be for the middle model
-	    modelMap.put(null, new ResourceImpl());
+	    modelMap.put(MIDDLE_MODEL, new ResourceImpl());
 	}
 	
 	/**
 	 * Adds the model to the list of models managed by this domain manager. The
-	 * domain manager supports only one root resource per typed model, this means that 
+	 * domain manager supports only one root resource per typed model, this means that
 	 * if a model was already binded to the TypedModel it will be replaced.
 	 *
-	 * @param coreModel the CoreModel that contains the QVT transformation 
 	 * @param typedModel the type model associated to the model
-	 * @param model the resource  
-	 * @param mmEcore2Pivot the pivot resource for the model's metamodel
-	 * @throws IllegalArgumentException if the metamodel does not have at least one root package
+	 * @param model the resource
 	 */
 	// TODO support multiple model instances by alias
-	public void addModel(TypedModel typedModel, @NonNull Resource model) {
+	public void addModel(@NonNull TypedModel typedModel, @NonNull Resource model) {
 		
 	    modelMap.put(typedModel, model);
 	}
@@ -82,7 +84,7 @@ public class QVTcDomainManager implements DomainModelManager {
 	
 	
 	/**
-	 * Gets the resources for all the models
+	 * Gets the resources for all the models.
 	 *
 	 * @return a collection of all the resources
 	 */
@@ -97,8 +99,45 @@ public class QVTcDomainManager implements DomainModelManager {
 	 * @return the middle model
 	 */
 	public Resource getMiddleModel() {
-		return modelMap.get(null);
+		return modelMap.get(MIDDLE_MODEL);
 	}
+	
+    
+	
+    /**
+     * Adds the model element to the resource of the given TypeModel
+     *
+     * @param tm the TypeModel
+     * @param element the element
+     * @return true, if successful
+     */
+    public boolean addModelElement(@Nullable TypedModel tm, @NonNull Object element) {
+        
+        Resource r = modelMap.get(tm);
+        return r.getContents().add((EObject) element);
+    }
+
+    
+    /**
+     * Gets the all the instances of the specified Type in the given TypeModel
+     *
+     * @param tm the TypeModel (can be null if it is the middle model)
+     * @param type the type of the elements that are retrieved
+     * @return the instances
+     */
+    public Set<Object> getInstances(@Nullable TypedModel tm, @NonNull Type type) {
+        
+        Set<Object> objectSet = new HashSet<>();
+        
+        for (TreeIterator<EObject> contents = modelMap.get(tm).getAllContents(); contents.hasNext();) {
+            Object object = contents.next();
+            //object.getClass().getName().equals(var.getType().getName())
+            if (object.getClass().equals(type)) {
+                objectSet.add(object);
+            }
+        }
+        return objectSet;
+    }
 
 
 	/**
@@ -109,11 +148,11 @@ public class QVTcDomainManager implements DomainModelManager {
 		for (Map.Entry<TypedModel, Resource> entry : modelMap.entrySet()) {
 		    Resource model = entry.getValue();
 		    if(entry.getKey() != null) {      // Don't sabe the middle model
-		        try{
+		        try {
 	                Map<Object, Object> options = new HashMap<Object, Object>();
 	                options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
 	                model.save(options);
-	               }catch (IOException e) {
+	               } catch (IOException e) {
 	                  e.printStackTrace();
 	               }
 		    }
@@ -128,7 +167,7 @@ public class QVTcDomainManager implements DomainModelManager {
 		try{
 			Map<Object, Object> options = new HashMap<Object, Object>();
 	    	options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
-	    	Resource model = modelMap.get(NULL);
+	    	Resource model = modelMap.get(MIDDLE_MODEL);
 	    	model.save(null);
 	       }catch (IOException e) {
 	          e.printStackTrace();
@@ -144,6 +183,9 @@ public class QVTcDomainManager implements DomainModelManager {
 		
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ocl.examples.domain.evaluation.DomainModelManager#get(org.eclipse.ocl.examples.domain.elements.DomainType)
+	 */
 	@Override
 	public @NonNull Set<EObject> get(@NonNull DomainType type) {
 		// TODO Auto-generated method stub
@@ -151,15 +193,15 @@ public class QVTcDomainManager implements DomainModelManager {
 	}
 	
 	/**
-     * Implemented by subclasses to determine whether the specified element
-     * is an instance of the specified class, according to the metamodel
-     * semantics implemented by the environment that created this extent map.
-     * 
-     * @param cls a class in the model
-     * @param element a potential run-time (M0) instance of that class
-     * @return <code>true</code> if this element is an instance of the given
-     *    class; <code>false</code> otherwise
-     */
+	 * Implemented by subclasses to determine whether the specified element
+	 * is an instance of the specified class, according to the metamodel
+	 * semantics implemented by the environment that created this extent map.
+	 *
+	 * @param type the type
+	 * @param element a potential run-time (M0) instance of that class
+	 * @return <code>true</code> if this element is an instance of the given
+	 * class; <code>false</code> otherwise
+	 */
 	protected boolean isInstance(@NonNull DomainType type, @NonNull EObject element) {
 		return false;
 	}
